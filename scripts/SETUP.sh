a2865782764505c5a3cd99208e30b5181636fb3f
hyouteki <lakshay21060@iiitd.ac.in>
2025-01-05 12:16:18 +0530

    use symlinks instead of copy

---
+++ scripts/SETUP.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# I know I can use GNU stow but fuck it we ball
+
+if [ -z $ROOT ]; then
+    ROOT=$(pwd)
+    echo "ROOT not set, defaulting to: ${ROOT}"
+fi
+
+ln -s -f $ROOT/i3/config                          ~/.config/i3/config
+ln -s -f $ROOT/i3status/config                    ~/.config/i3status/config
+ln -s -f $ROOT/gestures/libinput-gestures.conf    ~/.config/libinput-gestures.conf
+ln -s -f $ROOT/tmux/config                        ~/.tmux.conf
+ln -s -f $ROOT/emacs/init.el                      ~/.emacs
+ln -s -f $ROOT/emacs/custom.el                    ~/.emacs.custom.el
