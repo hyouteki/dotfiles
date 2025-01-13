c074363d2e504c0f3ba4948396f3889103676adc
hyouteki <lakshay21060@iiitd.ac.in>
2025-01-13 16:39:23 +0530

    new fish config

---
+++ fish/config.fish
@@ -0,0 +1,4 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+abbr -a -- enw 'emacs -nw'
--- scripts/SETUP.sh
+++ scripts/SETUP.sh
@@ -10,6 +10,7 @@ fi
 ln -s -f $ROOT/i3/config                          ~/.config/i3/config
 ln -s -f $ROOT/i3status/config                    ~/.config/i3status/config
 ln -s -f $ROOT/gestures/libinput-gestures.conf    ~/.config/libinput-gestures.conf
+ln -s -f $ROOT/fish/config.fish                   ~/.config/fish/config.fish
 ln -s -f $ROOT/tmux/config                        ~/.tmux.conf
 ln -s -f $ROOT/emacs/init.el                      ~/.emacs
 # NOTE: symlink does not work here for some reason
